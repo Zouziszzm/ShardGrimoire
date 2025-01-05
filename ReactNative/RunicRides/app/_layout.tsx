0eaaa21283e6ca9892e88543b2f4ed5f98dfe74c
Al Farhaan khan I Inamdar <63239385+Zouziszzm@users.noreply.github.com>
2025-01-05 23:37:22 +0300

    revocation

--- ReactNative/RunicRides/app/_layout.tsx
+++
@@ -1,6 +0,0 @@
-import { Stack } from "expo-router";
-import "@/app\\global.css";
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-export default function RootLayout() {
-  return <GluestackUIProvider mode="light"><Stack /></GluestackUIProvider>;
-}
